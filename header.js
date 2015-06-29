c9e2f668b83912f9c5f28ef92afb302350f70f76
ClarenceWoo <599260872@qq.com>
2015-06-29 17:53:26 +0800

    IE websocket debugged

--- header.js
+++ header.js
@@ -1,7 +1,15 @@
-if (typeof exports === 'undefined'){
+/*if (typeof exports === 'undefined'){
 	this.address = 'int.clarencewoo.com';
 	this.port = 8083;
 } else {
 	exports.port = 8083;
 	exports.address = '104.236.18.111';
+}
+*/
+if (typeof exports === 'undefined'){
+	this.address = '127.0.0.1';
+	this.port = 8083;
+} else {
+	exports.port = 8083;
+	exports.address = '127.0.0.1';
 }
