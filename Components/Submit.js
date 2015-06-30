d957227ecd896acb80879906d15166f5a5950933
ClarenceWoo <599260872@qq.com>
2015-06-30 18:16:05 +0800

    improved shield effect

--- Components/Submit.js
+++ Components/Submit.js
@@ -185,7 +185,7 @@ p.putMissile = function(event){
 		var s2 = new createjs.Shape();
 		s2.graphics.setStrokeStyle(2, 'round', 'round').beginStroke("#293047").arc(missile.graphics.command.x, missile.graphics.command.y, 22, 0, 2*Math.PI);
 		s2.alpha = 0;
-		shields.push(s2);
+		shields[1] = s2;
 		/*
 		var s3 = new createjs.Shape();
 		s3.graphics.setStrokeStyle(2, 'round', 'round').beginStroke("#293047").arc(missile.graphics.command.x, missile.graphics.command.y, 40, 0, 2*Math.PI);
